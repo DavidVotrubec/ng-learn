242e24139a7ab725fd0204fc738e0ba81183edc5
David Votrubec <david.votrubec@gmail.com>
2017-12-18 20:55:51 +0100

    Added test component. Added sass stylesheet with import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule
