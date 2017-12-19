ea19753c9d900290b9d138979b33e153899680f5
David Votrubec <david.votrubec@gmail.com>
2017-12-19 13:08:51 +0100

    Added draft of search box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorPickerComponent
+    ColorPickerComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule
