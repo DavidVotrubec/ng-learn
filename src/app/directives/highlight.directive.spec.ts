6e7be08e8f4818a31ac22833f089155fccc35eb7
David Votrubec <david.votrubec@gmail.com>
2017-12-29 13:52:05 +0100

    Fixed failing test

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,11 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef } from '@angular/core';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const span = document.createElement('span');
+    const elementRef = new ElementRef(span);
+    const directive = new HighlightDirective(elementRef);
     expect(directive).toBeTruthy();
   });
 });
